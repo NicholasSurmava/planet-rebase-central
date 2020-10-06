b9bfef72095d54c93f02b813bd5d87f29898f34d
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-10-06 17:58:19 -0500

    added a few routes for the site report ssr page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const http = require("http");
 const server = http.Server(app);
+const querystring = require("querystring");
 
-app.get('/', (req, res) => {
-    res.send('Hello! :)')
-})
+app.get("/", (req, res) => {
+  res.redirect("/site_status");
+});
+
+app.get("/site_status", (req, res) => {
+  res.send("search site");
+});
+
+app.get("/site_status/report", (req, res) => {
+  if (req.query.site_id) {
+    const query = querystring.stringify({
+      site_id: req.query.site_id,
+    });
+    res.send(`Site id: ${req.query.site_id}`);
+  } else {
+    res.send(404);
+  }
+});
 
 module.exports = server;

d731014c1023a3ae51ad3ca9fc8264f710d83357
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-10-07 10:08:06 -0500

    changed server console log message

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./src/index");
 const port = process.env.PORT || 8080
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Server listening at http://localhost:${port}`)
 })
 
 process.on("unhandledRejection", e => {

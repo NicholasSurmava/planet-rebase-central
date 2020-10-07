8b5cc1f5c6cfc810677002a4d7d5379f27d3e11c
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-10-07 10:09:13 -0500

    added support for ejs layouts, created a fullwidth layout

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,20 +4,25 @@ const http = require("http");
 const server = http.Server(app);
 const querystring = require("querystring");
 const path = require("path");
+const expressLayouts = require('express-ejs-layouts')
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("public/build"));
 
 // console.log(express.static);
-
+app.use(expressLayouts);
+app.set('layout', './layouts/full_width_layout')
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   // * redirect to the site_status page for now
   // res.redirect("/site_status");
+
+    //   TODO: Turn into an object
   let links = ["/site_status", "/api/v1/warehouse", "/heartbeat"];
+
   res.render("temp", { links: links });
 });
 

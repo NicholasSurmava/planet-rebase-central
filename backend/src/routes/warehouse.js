5b0e1062f37355c53402e102b20f7bca286c0b90
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-10-09 19:05:45 -0500

    made further changes and added some more demo data... really need to refactor this.. ugh

--- backend/src/routes/warehouse.js
+++ backend/src/routes/warehouse.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-notes = {
-  "note": "I logged into the location and have confirmed a problem!"
-}
-
+notes = [
+  {
+    author: "tech #1",
+    note: "Officia incididunt aliqua cillum excepteur aliqua.",
+  },
+  {
+    author: "tech 2",
+    note: "Ipsum consectetur mollit nulla ea fugiat non veniam do fugiat.",
+  },
+];
 
 // API ROUTES
 router.get("/warehouse", (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    setTimeout(() => {  res.send(JSON.stringify( notes )); }, 1000);
-  });
+  res.setHeader("Content-Type", "application/json");
+  setTimeout(() => {
+    res.send(JSON.stringify(notes));
+  }, 1000);
+});
 
 module.exports = router;

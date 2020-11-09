d7b377acab7572b3cbe77a41620336ce4b624928
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-11-08 21:39:33 -0600

    added some basic configuration while i try to figure out what i'm going to do with this...

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,42 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+console.log(app.locals.settings);
+
+// controller
 app.get("/", (req, res, next) => {
-  console.log("Processing....");
+  // model
+  let metaData = {
+    title: "Home",
+  };
+
+  // services
+  setTimeout(() => {
+    console.log("Gathering some data... 10%");
+  }, 2000);
 
   setTimeout(() => {
-    console.log("100% Complete.");
-    res.send({ message: "hello world!" });
-  }, 2000);
+    console.log("Gathering some data... 20%");
+  }, 4000);
+
+  setTimeout(() => {
+    console.log("Gathering some data... 60%");
+  }, 6000);
+
+  setTimeout(() => {
+    console.log("Gathering some data... 80%");
+  }, 8000);
+
+  setTimeout(() => {
+    console.log("Here you go! 100% Complete.");
+    // View
+    res.render("index", { message: "hello world", metaData });
+  }, 10000);
 });
 
 module.exports = app;

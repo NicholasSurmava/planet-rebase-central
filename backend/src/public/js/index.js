306164493dbf8baac67357042659ad3ad64608f6
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-11-10 23:47:20 -0600

    imported loader/spinner css, test script to just demo it

--- backend/src/public/js/index.js
+++ backend/src/public/js/index.js
@@ -1,4 +1,5 @@
 import "../../../node_modules/bulma/css/bulma.min.css";
+import "../css/spinner.css";
 
 // let checkExistence = (el) => {
 //   let element = document.querySelector(el);
@@ -54,17 +55,53 @@ import "../../../node_modules/bulma/css/bulma.min.css";
 //   });
 // })();
 
-let hideShowTest = () => {
-  let progressBar = document.querySelector('[data-js="progressBar"]');
-  let summarySection = document.querySelector('[data-js="summarySection"]');
+// let hideShowTest = () => {
+//   let progressBar = document.querySelector('[data-js="progressBar"]');
+//   let summarySection = document.querySelector('[data-js="summarySection"]');
+
+//   setTimeout(() => {
+//     progressBar.classList.add("is-hidden");
+//     summarySection.classList.remove("is-hidden");
+//   }, 5000);
+// };
+
+let test = () => {
+  let activeAlarmContainer = document.querySelector(
+    '[data-js="activeAlarmContainer"]'
+  );
+  let openIncidentContainer = document.querySelector(
+    '[data-js="openIncidentContainer"]'
+  );
+  let openMaintenanceContainer = document.querySelector(
+    '[data-js="openMaintenanceContainer"]'
+  );
+  let siteSignInContainer = document.querySelector(
+    '[data-js="siteSignInContainer"]'
+  );
 
   setTimeout(() => {
-    progressBar.classList.add("is-hidden");
-    summarySection.classList.remove("is-hidden");
-  }, 5000);
+    activeAlarmContainer.classList.remove("loader");
+    activeAlarmContainer.innerHTML = "0";
+  }, 2000);
+
+  setTimeout(() => {
+    openIncidentContainer.classList.remove("loader");
+    openIncidentContainer.innerHTML = "10";
+  }, 4000);
+
+  setTimeout(() => {
+    openMaintenanceContainer.classList.remove("loader");
+    openMaintenanceContainer.innerHTML = "4";
+  }, 1000);
+
+  setTimeout(() => {
+    siteSignInContainer.classList.remove("loader");
+    siteSignInContainer.innerHTML = "1";
+  }, 10000);
 };
 
 window.onload = () => {
-  hideShowTest();
+  // hideShowTest();
   //   console.log(getActiveTab(tabs));
+  test();
 };

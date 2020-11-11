17c1f20686756dd1d94eda7de19dd99e88411042
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-11-10 22:18:04 -0600

    started work on the front-end func for bulma

--- backend/src/public/js/index.js
+++ backend/src/public/js/index.js
@@ -1,1 +1,70 @@
 import "../../../node_modules/bulma/css/bulma.min.css";
+
+// let checkExistence = (el) => {
+//   let element = document.querySelector(el);
+//   if (typeof element != "undefined" && element != null) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// TODO: Create a Tab object/class w/ methods for switching tabs, setting active tab, remove active tab, get active tab
+// let setActiveTab = (tab) => {
+//     tab.classList()
+
+// }
+
+// let getActiveTab = (tabs) => {
+//   let activeTab;
+//   //   console.log(tabs);
+//   tabs.forEach((tab) => {
+//     if (tab.nodeName === "LI" && tab.className === "is-active") {
+//       //   console.log("hello");
+//       //   console.log(tab);
+//       activeTab = tab;
+//     }
+//   });
+//   return activeTab;
+// };
+// let tabs = document.querySelector('[data-js="tabs"]').childNodes;
+
+// console.log(tabs);
+// console.log(getActiveTab(tabs));
+
+// console.log(tabs);
+
+// let activeNode;
+
+// let tabSwticher = (function () {
+//   let tabs = document.querySelector('[data-js="tabs"]').childNodes;
+
+//   tabs.forEach((tab) => {
+//     if (tab.nodeName === "LI") {
+//       if (tab.className === "is-active") {
+//         activeTab = tab;
+//       }
+//       tab.addEventListener("click", () => {
+//         if (tab.className != "is-active") {
+//           tab.classList.add("is-active");
+//           activeTab.classList.remove("is-active");
+//         }
+//       });
+//     }
+//   });
+// })();
+
+let hideShowTest = () => {
+  let progressBar = document.querySelector('[data-js="progressBar"]');
+  let summarySection = document.querySelector('[data-js="summarySection"]');
+
+  setTimeout(() => {
+    progressBar.classList.add("is-hidden");
+    summarySection.classList.remove("is-hidden");
+  }, 5000);
+};
+
+window.onload = () => {
+  hideShowTest();
+  //   console.log(getActiveTab(tabs));
+};

48979f8d0ef757a2f718b3d4363e92baec265257
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-11-13 22:41:51 -0600

    added chart.js demo, map demo, and added details tab placeholder data

--- backend/src/public/js/index.js
+++ backend/src/public/js/index.js
@@ -1,9 +1,12 @@
 import "../../../node_modules/bulma/css/bulma.min.css";
+import "../../../node_modules/chart.js/dist/Chart.min.css";
+import "../../../node_modules/chart.js/dist/Chart.min.js";
 import "../css/spinner.css";
 import "../css/map_leaflet.css";
+import { routes } from "../../../config";
 
 import { Tabs, TabbedSections } from "./tabs";
-import Mapper from "./Mapper";
+// import Mapper from "./Mapper";
 
 export let checkExistence = (element) => {
   // let element = document.querySelector(el);
@@ -11,6 +14,97 @@ export let checkExistence = (element) => {
     return true;
   } else {
     return false;
+  }
+};
+
+// TODO: Create a factory function to return an initialized map object, export said object and import as needed. Example: import into tabs component to call the rerender method when map tab is clicked.
+let mapDemo = () => {
+  let mapContainer = document.getElementById("mapid");
+
+  if (checkExistence(mapContainer) === true) {
+    let mymap = L.map(mapContainer);
+    mymap.setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(mymap);
+
+    var circle = L.circle([51.505, -0.09], {
+      color: "red",
+      fillColor: "#f03",
+      fillOpacity: 0.5,
+      radius: 100,
+    })
+      .addTo(mymap)
+      .on("click", onClick);
+
+    function onClick(e) {
+      const URL = `${routes.tower_360_report}?site=123456`;
+      window.open(URL, "_blank");
+    }
+
+    var popup = L.popup();
+
+    circle.on("mouseover", function () {
+      circle.bindPopup("Site id: 12345").openPopup();
+    });
+
+    circle.on("mouseout", function () {
+      circle.closePopup();
+    });
+
+    setTimeout(function () {
+      mymap.invalidateSize();
+    }, 4000);
+  }
+};
+
+let ChartInit = () => {
+  var ctx = document.getElementById("myChart");
+
+  if (checkExistence(ctx) === true) {
+    let ctx2 = ctx.getContext("2d");
+    var myChart = new Chart(ctx2, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
   }
 };
 
@@ -61,4 +155,6 @@ window.onload = () => {
   test();
   Tabs();
   TabbedSections();
+  mapDemo();
+  ChartInit();
 };

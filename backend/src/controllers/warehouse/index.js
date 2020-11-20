7115152bc2980a4e3cbc74c9cd050b454a6b1efd
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-11-19 21:26:50 -0600

    added warehouse service functions to warehouse controller

--- backend/src/controllers/warehouse/index.js
+++ backend/src/controllers/warehouse/index.js
@@ -1,33 +1,12 @@
 // TODO: Routes for warehouse (api)
 const router = require("express").Router();
 const { routes } = require("../../../config");
-var faker = require("faker");
-
-const { Site } = require("../../models");
-const { User } = require("../../models");
+const faker = require("faker");
+const { getAllSites, getAllMWSites } = require("../../services/warehouse");
 
 router.get(routes.index + "get_sites", async (req, res, next) => {
   try {
-    let newUser = new User(
-      faker.name.firstName(),
-      faker.name.lastName(),
-      faker.internet.userName(),
-      faker.internet.password()
-    );
-    let city = faker.address.city();
-    let data = new Site(
-      1,
-      (location = {
-        street_address: faker.address.streetAddress(),
-        city: city,
-        state: faker.address.state(),
-        zip: faker.address.zipCode(),
-      }),
-      city,
-      111111,
-      (tech = ["low_speed_data", "high_speed_data", "voice", "emergency"]),
-      newUser.userName
-    );
+    let data = getAllSites();
 
     res.json({ data });
   } catch (e) {
@@ -36,3 +15,12 @@ router.get(routes.index + "get_sites", async (req, res, next) => {
 });
 
 module.exports = router;
+
+router.get(routes.index + "get_mw", async (req, res, next) => {
+  try {
+    let data = getAllMWSites();
+    res.json({ data });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});

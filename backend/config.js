29f952635551d482c3c5fd131a2af316c88331cf
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-01-15 17:40:28 -0600

    added warehouse_get routes in config

--- backend/config.js
+++ backend/config.js
@@ -4,4 +4,6 @@ exports.routes = {
   tower_360: "/tower_360",
   tower_360_report: "/tower_360/report",
   warehouse: "/warehouse",
+  warehouse_get_sites: "/warehouse/get_sites",
+  warehouse_get_mw: "/warehouse/get_mw",
 };

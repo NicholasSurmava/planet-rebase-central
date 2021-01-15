c7d15d664d9c23fdae2b3c109b08025c0e3b4ac2
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-01-15 17:41:00 -0600

    fixed routes for warehouse

--- backend/src/Warehouse/controllers/index.js
+++ backend/src/Warehouse/controllers/index.js
@@ -1,10 +1,9 @@
 // TODO: Routes for warehouse (api)
 const router = require("express").Router();
 const { routes } = require("../../../config");
-const faker = require("faker");
 const { getAllSites, getAllMWSites } = require("../services");
 
-router.get(routes.index + "get_sites", async (req, res, next) => {
+router.get(routes.warehouse_get_sites, async (req, res, next) => {
   try {
     let data = getAllSites();
 
@@ -14,9 +13,7 @@ router.get(routes.index + "get_sites", async (req, res, next) => {
   }
 });
 
-module.exports = router;
-
-router.get(routes.index + "get_mw", async (req, res, next) => {
+router.get(routes.warehouse_get_mw, async (req, res, next) => {
   try {
     let data = getAllMWSites();
 
@@ -25,3 +22,5 @@ router.get(routes.index + "get_mw", async (req, res, next) => {
     res.status(500).json({ error: e.message });
   }
 });
+
+module.exports = router;

05769908ccdae0c5675698df4b707c9060377504
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-07-06 14:49:47 -0500

    project restart

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./src');
-const PORT = 8080;
-
-app.listen(PORT, ()=> {
-    console.log(`Server has started on PORT: ${PORT}`);
-});
--- backend/src/models/index.js
+++
@@ -1,8 +0,0 @@
-const User = function (firstName, lastName, userName, password) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.userName = userName;
-  this.password = password;
-};
-
-module.exports = { User };
